a53399c4f3dbbf2b81485deabdea47da0bf543df
amin <aminmosavi4433@gmail.com>
2025-05-02 18:49:59 +0330

    *

--- src/apps/home/views.py
+++ src/apps/home/views.py
@@ -4,7 +4,5 @@ from apps.accounts.models import Profile
 
 
 def home(request):
-    profile = get_object_or_404(Profile, user=request.user)
 
-    context = {'profile': profile}
-    return render(request,'home/home.html',context)
+    return render(request,'home/home.html')
