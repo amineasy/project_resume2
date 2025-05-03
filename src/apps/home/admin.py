6caf21cf5b9bbfeb79eca6227bb9576dc1342d71
amin <aminmosavi4433@gmail.com>
2025-05-03 16:22:08 +0330

    create models for product

--- src/apps/home/admin.py
+++ src/apps/home/admin.py
@@ -1,11 +1,54 @@
 from django.contrib import admin
+from treebeard.forms import movenodeform_factory
+
 from .models import *
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    form = movenodeform_factory(Category)
     list_display = ('title', 'slug')
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title',)
 
 
+
+class ProductClassAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
+
+
+class ProductAttributeInline(admin.TabularInline):
+    model = ProductAttribute
+    extra = 1
+
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('total_price',)
+    inlines = [ProductAttributeInline]
+
+
+
+class ProductColorAdmin(admin.ModelAdmin):
+    list_display = ('color',)
+
+
+
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = ('size',)
+
+
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductClass, ProductClassAdmin)
+admin.site.register(ProductColor, ProductColorAdmin)
+admin.site.register(ProductSize, ProductSizeAdmin)
+admin.site.register(ProductAttribute)
--- src/apps/home/models.py
+++ src/apps/home/models.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from django.db import models
 from treebeard.mp_tree import MP_Node
 
@@ -9,3 +11,132 @@ class Category(MP_Node):
 
     def __str__(self):
         return self.title
+
+
+
+
+
+class ProductClass(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+class Product(models.Model):
+    product_class = models.ForeignKey(ProductClass, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    price = models.IntegerField(blank=True,null=True)
+    discount_price = models.IntegerField(blank=True,null=True)
+    total_price = models.IntegerField(blank=True,null=True)
+    quantity = models.IntegerField(default=1)
+
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True,null=True)
+
+    def get_total_price(self):
+        if self.discount_price:
+            discount_amount = (self.discount_price / 100) * self.price
+            return self.price - discount_amount
+        return self.price
+
+
+    def __str__(self):
+        return self.title
+
+
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.get_total_price()
+        super().save(*args, **kwargs)
+
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.ForeignKey('ProductColor', on_delete=models.CASCADE,blank=True, null=True)
+    size = models.ForeignKey('ProductSize', on_delete=models.CASCADE,blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.IntegerField(blank=True)
+    discount_price = models.IntegerField(blank=True, null=True)
+    total_price = models.IntegerField(blank=True, null=True)
+
+    def get_total_price(self):
+        if self.price is not None:
+            base_price = self.price
+        else:
+            base_price = self.product.price
+
+        if self.discount_price is not None:
+            discount = self.discount_price
+        else:
+            discount = self.product.discount_price
+
+        if discount:
+            discount_amount = (discount / 100) * base_price
+            return base_price - discount_amount
+
+        return base_price
+
+
+    def get_price(self):
+        if self.price is not None:
+            return self.price
+        else:
+            self.price = self.product.price
+            return self.price
+
+
+    def get_discount_price(self):
+        if self.price == self.product.price:
+            self.discount_price = self.product.discount_price
+            return self.discount_price
+        else:
+            return None
+
+
+
+
+
+
+    def __str__(self):
+        return self.product.title
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.get_total_price()
+        self.price = self.get_price()
+        self.discount_price = self.get_discount_price()
+
+        super().save(*args, **kwargs)
+
+
+
+
+
+
+
+
+
+class ProductColor(models.Model):
+    color = models.CharField(max_length=100,null=True,blank=True)
+
+
+    def __str__(self):
+        return self.color
+
+
+
+class ProductSize(models.Model):
+    size = models.CharField(max_length=100,null=True,blank=True)
+
+
+    def __str__(self):
+        return self.size
+
+
+
